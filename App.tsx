9f78e4ba62c4ddd78e195b0c72cf2a7778d218c6
Kelly <141122532+rmsoft-kelly@users.noreply.github.com>
2024-06-27 17:09:40 +0900

    fix: remove unusing codes

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
